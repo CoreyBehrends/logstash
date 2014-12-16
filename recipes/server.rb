522e4878c46076177066b4d4ab89eee4e04eeb73
Corey Behrends <cbehrends@beapro.com>
2014-12-16 13:44:51 -0600

    Fix permission

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,9 +6,11 @@
 # 
 # License: MIT
 #
-
 template "/etc/logstash/conf.d/server.conf" do
   source "logstash.conf.erb"
+  owner "logstash"
+  group "logstash"
+  mode "0644"
   variables( :config => node[:logstash][:server] )
   notifies :restart, "service[logstash]"
 end
